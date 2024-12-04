dcd8cf8d305b1288489dfd0719bfe4b07d0e7180
Aneesh <aneeshchandran06@gmail.com>
2024-12-04 20:40:04 +0530

    deploy changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "testreact",
   server: {
-    open: true,  // Ensures the browser opens automatically
-  }
-})
+    open: true, // Ensures the browser opens automatically
+  },
+});
